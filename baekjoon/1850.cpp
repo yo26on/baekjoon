1632b671384c9c5e7a094ce1be6a146a921a6cdd
김윤서 <>
2021-01-14 00:29:24 +0900

    2020/01/14

---
+++ baekjoon/1850.cpp
@@ -0,0 +1,28 @@
+//
+//  1850.cpp
+//  baekjoon
+//
+//  Created by 김윤서 on 2021/01/13.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long long gcd(long long a, long long b) {
+    if (a % b == 0)
+        return b;
+    else
+        return gcd(b, a%b);
+}
+ 
+int main() {
+    long long a, b;
+    cin>>a>>b;
+    
+    long long g = a > b ? gcd(a, b) : gcd(b, a);
+    for(long long i = 0 ; i<g ; i++){
+        cout<<1;
+    }
+    return 0;
+}
